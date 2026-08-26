ef9eb108d08be196323aeb871e1b94e98c378cf0
Jonathan Powell <jprophit@gmail.com>
2026-08-26 23:59:47 +0100

    Remove tracked build and cache artifacts

--- build/.compiled-core-stale-20260822-1602/community/contracts.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COMMUNITY_FOUNDATION_SCHEMA_VERSION = void 0;
-exports.COMMUNITY_FOUNDATION_SCHEMA_VERSION = "1.0.0";
--- build/.compiled-core-stale-20260822-1602/engines/contracts.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PRODUCTION_ENGINE_EXTENSION_KEY = exports.PRODUCTION_ENGINE_SCHEMA_VERSION = void 0;
-exports.PRODUCTION_ENGINE_SCHEMA_VERSION = '1.0.0';
-exports.PRODUCTION_ENGINE_EXTENSION_KEY = 'org.poietek.production-engines';
